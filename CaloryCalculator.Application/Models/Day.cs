75eda37ebed2ea67c5b27a023fcdfa96524fa8da
skucherenko <kucherenkoserhiy@gmail.com>
2023-03-04 21:53:07 +0100

    [refact] fixed namespace

--- CaloryCalculator.Application/Models/Day.cs
+++ CaloryCalculator.Application/Models/Day.cs
@@ -1,10 +1,11 @@
-﻿using CaloryCalculator.Application.Models;
-
-public class Day
+﻿namespace CaloryCalculator.Application.Models
 {
-    public DateTime Date { get; set; }
-    public double TotalCaloriesConsumed { get; set; }
-    public double TotalCaloriesBurned { get; set; }
-    public List<Product> Consumed { get; set; } = new List<Product>();
-    public List<Exercise> Performed { get; set; } = new List<Exercise>();
+    public class Day
+    {
+        public DateTime Date { get; set; }
+        public double TotalCaloriesConsumed { get; set; }
+        public double TotalCaloriesBurned { get; set; }
+        public List<Product> Consumed { get; set; } = new List<Product>();
+        public List<Exercise> Performed { get; set; } = new List<Exercise>();
+    }
 }
