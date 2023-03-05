fa557cd69ffe724ef62529131a0cf38f9c1ea8ba
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-03-05 03:22:19 +0100

    [chore] set c# version to 10

--- CalorieTracker.Application/Enums/ActivityType.cs
+++ CalorieTracker.Application/Enums/ActivityType.cs
@@ -1,13 +1,14 @@
-﻿namespace CalorieCalculator.Application.Enums;
-
-public enum ActivityType
+﻿namespace CalorieTracker.Application.Enums
 {
-    NoExerciseSittingJob,
-    LightWorkoutSittingJob, // 1-2 times per week
-    LightWorkoutActiveJob, // 1-2 times per week
-    ModerateWorkoutSittingJob, // 3-5 times per week
-    ModerateWorkoutActiveJob, // 3-5 times per week
-    HighWorkoutSittingJob, // 6-7 times per week
-    HighWorkoutActiveJob, // 6-7 times per week
-    EliteWorkoutVeryActive
+    public enum ActivityType
+    {
+        NoExerciseSittingJob,
+        LightWorkoutSittingJob, // 1-2 times per week
+        LightWorkoutActiveJob, // 1-2 times per week
+        ModerateWorkoutSittingJob, // 3-5 times per week
+        ModerateWorkoutActiveJob, // 3-5 times per week
+        HighWorkoutSittingJob, // 6-7 times per week
+        HighWorkoutActiveJob, // 6-7 times per week
+        EliteWorkoutVeryActive
+    }
 }
