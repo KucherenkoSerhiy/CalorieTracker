1685255180dc87565e76c26c35bc2743b7478bed
skucherenko <kucherenkoserhiy@gmail.com>
2023-03-05 02:12:54 +0100

    [feat] added repository implementations

---
+++ CalorieCalculator.Infrastructure/Services/DayRepository.cs
@@ -0,0 +1,38 @@
+﻿using CalorieCalculator.Application.Models;
+using CalorieCalculator.Application.Repositories;
+using CalorieCalculator.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCalculator.Infrastructure.Services;
+
+public class DayRepository : IDayRepository
+{
+    private readonly DataContext dataContext;
+
+    public DayRepository(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    public async Task<Day?> GetDayAsync(DateTime date)
+    {
+        return await dataContext.Days
+            .Include(d => d.Consumed)
+            .Include(d => d.Performed)
+            .FirstOrDefaultAsync(d => d.Date == date);
+    }
+
+    public async Task<List<Day>> GetDaysAsync(DateTime startDate, DateTime endDate)
+    {
+        return await dataContext.Days
+            .Include(d => d.Consumed)
+            .Include(d => d.Performed)
+            .Where(d => d.Date >= startDate && d.Date < endDate).ToListAsync();
+    }
+
+    public async Task SaveDayAsync(Day day)
+    {
+        await dataContext.Days.AddAsync(day);
+        await dataContext.SaveChangesAsync();
+    }
+}
---
+++ CalorieCalculator.Infrastructure/Services/PersonRepository.cs
@@ -0,0 +1,27 @@
+﻿using CalorieCalculator.Application.Models;
+using CalorieCalculator.Application.Repositories;
+using CalorieCalculator.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCalculator.Infrastructure.Services;
+
+public class PersonRepository : IPersonRepository
+{
+    private readonly DataContext dataContext;
+
+    public PersonRepository(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    public async Task<Person?> GetPersonAsync()
+    {
+        return await dataContext.People.FirstOrDefaultAsync();
+    }
+
+    public async Task SavePersonAsync(Person person)
+    {
+        await dataContext.People.AddAsync(person);
+        await dataContext.SaveChangesAsync();
+    }
+}
