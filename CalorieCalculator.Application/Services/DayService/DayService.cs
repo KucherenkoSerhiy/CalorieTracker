2d35934f88e7465ff9017d9863fd07d55ee92af8
skucherenko <kucherenkoserhiy@gmail.com>
2023-03-05 01:04:12 +0100

    [feat] implementing the services

---
+++ CalorieCalculator.Application/Services/DayService/DayService.cs
@@ -0,0 +1,42 @@
+﻿using CalorieCalculator.Application.Models;
+
+namespace CalorieCalculator.Application.Services.DayService;
+
+public class DayService : IDayService
+{
+    private readonly IDayRepository dayRepository;
+
+    public DayService(IDayRepository dayRepository)
+    {
+        this.dayRepository = dayRepository;
+    }
+
+    public async Task<Day> GetOrCreateDayAsync(DateTime date)
+    {
+        var day = await dayRepository.GetDayAsync(date);
+        if (day != null) return day;
+        
+        day = new Day { Date = date };
+        await dayRepository.SaveDayAsync(day);
+        return day;
+    }
+
+    public async Task<IReadOnlyCollection<Day>> GetLastNDays(int n)
+    {
+        var endDate = DateTime.Today.AddDays(1);
+        var startDate = endDate.AddDays(-n);
+        return await dayRepository.GetDaysAsync(startDate, endDate);
+    }
+
+    public async Task<IReadOnlyCollection<Day>> GetMonth(DateTime month)
+    {
+        var startDate = new DateTime(month.Year, month.Month, 1);
+        var endDate = startDate.AddMonths(1);
+        return await dayRepository.GetDaysAsync(startDate, endDate);
+    }
+
+    public async Task SaveDayAsync(Day day)
+    {
+        await dayRepository.SaveDayAsync(day);
+    }
+}
---
+++ CalorieCalculator.Application/Services/PersonService/PersonService.cs
@@ -0,0 +1,23 @@
+﻿using CalorieCalculator.Application.Models;
+
+namespace CalorieCalculator.Application.Services.PersonService;
+
+public class PersonService : IPersonService
+{
+    private readonly IPersonRepository personRepository;
+
+    public PersonService(IPersonRepository personRepository)
+    {
+        this.personRepository = personRepository;
+    }
+
+    public async Task<Person> GetPersonAsync()
+    {
+        var person = await personRepository.GetPersonAsync();
+        if (person != null) return person;
+        
+        person = new Person();
+        await personRepository.SavePersonAsync(person);
+        return person;
+    }
+}
