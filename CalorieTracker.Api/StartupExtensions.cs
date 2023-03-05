a0a84bc66ce5842d24e70dae8ccb743879194139
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-03-05 16:07:55 +0100

    [faet] clean up startup and regster services and repositories

---
+++ CalorieTracker.Api/StartupExtensions.cs
@@ -0,0 +1,41 @@
+﻿using CalorieTracker.Application.Repositories;
+using CalorieTracker.Application.Services.DayService;
+using CalorieTracker.Application.Services.PersonService;
+using CalorieTracker.Infrastructure.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CalorieTracker.Api
+{
+    public static class StartupExtensions
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IDayService, DayService>();
+            builder.Services.AddScoped<IPersonService, PersonService>();
+
+            builder.Services.AddScoped<IDayRepository, DayRepository>();
+            builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+        }
+    
+        public static void RegisterMiddleware(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
