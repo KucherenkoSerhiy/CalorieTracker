94fddc4b4a69c56106d478d239fd099321eab40f
skucherenko <kucherenkoserhiy@gmail.com>
2023-03-05 01:30:15 +0100

    [feat] added repository contracts

---
+++ CalorieCalculator.Application/Repositories/IDayRepository.cs
@@ -0,0 +1,10 @@
+﻿using CalorieCalculator.Application.Models;
+
+namespace CalorieCalculator.Application.Repositories;
+
+public interface IDayRepository
+{
+    Task<Day?> GetDayAsync(DateTime date);
+    Task<IReadOnlyCollection<Day>> GetDaysAsync(DateTime startDate, DateTime endDate);
+    Task SaveDayAsync(Day day);
+}
